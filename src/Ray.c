c18a19af453cecc097e598df3025bf4b999ac151
Alp Yilmaz <Hexa3333@protonmail.com>
2023-08-03 16:50:58 +0300

    Optimize Rays

--- src/Ray.c
+++ src/Ray.c
@@ -11,13 +11,13 @@ void CastRay3D(vec3s caste)
 		// The block vertices go from -.5f to +.5f,
 		// meaning x,y,z points to the middle of the block,
 		// therefore we offset them.
-		if (glms_vec3_distance2(g_MainCamera.position, p) < 1000 &&
-			p.x > caste.x - 0.5f && p.x < caste.x + 0.5f &&
+		if (p.x > caste.x - 0.5f && p.x < caste.x + 0.5f &&
 			p.y > caste.y - 0.5f && p.y < caste.y + 0.5f &&
 			p.z < caste.z + 0.5f && p.z > caste.z - 0.5f
 			)
 		{
 			LOG("INTERSECTION!");
+			return;
 		}
 		p = glms_vec3_add(p, glms_vec3_divs(g_MainCamera.front, 2));
 	}
